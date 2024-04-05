126f0ab6627b46229d21525ccf8ded4017ecec21
Filipe <filipesilva.emp@gmail.com>
2024-04-05 19:08:29 -0300

    Commit 1

--- App.js
+++ App.js
@@ -1,81 +1,80 @@
-import { Text, SafeAreaView, StyleSheet, View, Button } from 'react-native';
-//import { useState } from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useState} from 'react';
+import { View, Button, Text} from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer"; 
-
-const Stack = createNativeStackNavigator();
-const Tab =createBottomTabNavigator();
+import { createDrawerNavigator } from "@react-navigation/drawer";
+ 
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
-
-//const [logged, setLogged] = useState(false);
-
-const Login = ({navigation}) => {return (
-    <View>
-      <Text>Tela de login</Text>
-      <Button title="Login" onPress={() => navigation.navigate('Home')}/>
-      <Button title="Registrar" onPress={() => navigation.navigate('Registrar')}/>
-    </View>);}
-
-const Registrar = ({navigation}) => {return (
-    <View>
-      <Text>Tela de registro</Text>
-    </View>);}
-
-const Configuracoes = ({navigation}) => {return (
-    <View>
-      <Text>Tela de Configurações</Text>
-    </View>);}
-
-const Contatos = ({navigation}) => {return (
-    <View>
-      <Text>Tela de Contatos</Text>
-    </View>);}
-
-const Tabconst = () => {
-  return (
-      <Tab.Navigator>
-      <Tab.Screen name="Home" component={Drawernav} />
-      <Tab.Screen name="Avisos" component={Drawernav} />
-      </Tab.Navigator>
-  )
-}
-const Drawernav = () => {
-  return (
-    <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Configurações" component={Configuracoes} />
-        <Drawer.Screen name="Contatos" component={Contatos} />
-    </Drawer.Navigator>
-  )
-}
-    
-
-const Avisos = ({navigation}) => {return (
-    <View>
-      <Text>Tela de Avisos</Text>
-    </View>);}
-
-const Home = ({navigation}) => {return (
-    <View>
-      <Text>Tela de home</Text>
-      <Button title="Profile" onPress={() => navigation.navigate('Profile')}/>
-
-    </View>);}
  
-const Profile = ({navigation}) => {return (
+const Login = ({navigation, route})=>{
+  return(
     <View>
-      <Text>Tela de profile</Text>
-    </View>);}
+      <Button onPress = {()=>route.params.funcLogar(true)} title='Logar'/>
+      <Button onPress = {()=>navigation.navigate("Registrar")} title = 'Registrar'/>
+    </View>
+  )}
  
-export default function App() {return (
-    <NavigationContainer>
-    <Stack.Navigator>
-        <Stack.Screen name="Loginn" component={Login}/>
-        <Stack.Screen name="Registrar" component={Registrar}/>
-        <Stack.Screen name="Home" component={Tabconst}/>
-        <Stack.Screen name="Profile" component={Profile} />     
-      </Stack.Navigator>
-
-    </NavigationContainer>
-    );}
+const NavPerfil = ({navigation, route})=>{
+  return(
+    <View>
+      <Button onPress = {()=>navigation.navigate("Perfil")} title = ''/>
+    </View>
+  )}
+ 
+const Logout = ({navigation, route})=>{
+  return(
+    <View>
+      <Button onPress = {()=>route.params.funcLogar(false)} title='Logout'/>
+    </View>
+  )}
+ 
+const Registrar = ()=>{return(<Text>Registrar</Text>)}
+ 
+const Avisos = ()=>{return(<Text>Avisos</Text>)}
+ 
+const Perfil = ()=>{return(<Text>Perfil</Text>)}
+ 
+const Home = ({navigation, route})=>{return(
+  <View>
+  <Text>Home</Text>
+  <Button onPress = {()=>route.params.funcLogar(false)} title = "Logout" />
+  <Button onPress = {()=>navigation.navigate("Perfil")} title = "Perfil" />
+  </View>
+  )}
+ 
+const Config = ()=>{return(<Text>Config</Text>)}
+ 
+const Contatos = ()=>{return(<Text>Contatos</Text>)}
+ 
+const App = () => {
+  const [EstaLogado, setLogado] = useState(false);
+  return(    	
+    EstaLogado?(
+      <NavigationContainer>		
+        <Drawer.Navigator>			
+          <Drawer.Screen name="Home" component={()=>{return(				
+            <Tab.Navigator>      					
+	            <Tab.Screen name="Home_tab" options={{headerShown:false}} component={()=>{return(
+                <Stack.Navigator>      						
+                  <Stack.Screen name="Home" options={{headerShown:false}} component={Home}  initialParams = {{funcLogar : setLogado}}/>
+                  <Stack.Screen name="Perfil" component={Perfil} />      						
+                </Stack.Navigator>)}} />
+              <Tab.Screen name="Avisos" component={Avisos} />
+            </Tab.Navigator>)}} />			
+          <Drawer.Screen name="Config" component={Config} />
+          <Drawer.Screen name="Contatos" component={Contatos} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+      ):(     
+      <NavigationContainer>	 		
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} initialParams = {{funcLogar : setLogado}}/>
+          <Stack.Screen name="Registrar" component={Registrar}/>      	
+        </Stack.Navigator>    
+    </NavigationContainer>))
+  };
+ 
+export default App;
